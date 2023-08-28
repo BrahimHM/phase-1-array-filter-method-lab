ec6f3285d48d95307f8ca38164e502a81cebb36f
Brahim HM <brahimhm98@gmail.com>
2023-08-28 23:27:06 +0100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const findMatching = (drivers,match)=>{
+    return drivers.filter((element)=>element.toUpperCase()===match.toUpperCase())
+}
+
+const fuzzyMatch = (drivers,match)=>{
+  return drivers.filter((element)=> element.slice(0,match.length)===match)
+}
+
+const matchName=(drivers,match)=>{
+    return drivers.filter((element)=>element.name===match)
+}
